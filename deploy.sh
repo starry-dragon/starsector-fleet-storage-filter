28068300acc5fd8a72d9b4c62539d1c43d076b98
starry-dragon <null>
2025-08-28 15:04:38 -0700

    Removed some development artifacts I don't need

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-rm -rf                "$SS_PATH/mods/FleetStorageFilter"
-mkdir                 "$SS_PATH/mods/FleetStorageFilter"
-
-cp -r ./jars          "$SS_PATH/mods/FleetStorageFilter"
-cp ./mod_info.json    "$SS_PATH/mods/FleetStorageFilter/mod_info.json"
-cp ./LICENSE          "$SS_PATH/mods/FleetStorageFilter/LICENSE"
